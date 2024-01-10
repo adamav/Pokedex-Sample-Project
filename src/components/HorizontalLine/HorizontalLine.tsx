4e3ca1ce5cf64b3475bbcc9bed29acc15677f0fc
Adam Avetisian <adamavetisian@Adams-iMac.local>
2024-01-10 08:16:45 -0800

    Cleared test console log from component

--- src/components/HorizontalLine/HorizontalLine.tsx
+++ src/components/HorizontalLine/HorizontalLine.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { HorizontalLineDiv } from "./HorizontalLine.styled";
 const HorizontalLine = () =>{
 
-    console.log("test")
+    
     return (
 
         <HorizontalLineDiv />
