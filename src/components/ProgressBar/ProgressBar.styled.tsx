837e75cb68c9dd251f8676fbf467b237f8bbfdc1
Adam Avetisian <adamavetisian@Adams-iMac.local>
2024-01-10 09:27:05 -0800

    Implemment a progress bar component

---
+++ src/components/ProgressBar/ProgressBar.styled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ProgressBarDiv = styled.div`
+    
+    background-color:gray;
+    width:100%;
+    height:5px;
+    border-color:black;
+    border-width:1px;
+    border-style:solid;
+   
+`
---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ProgressBarDiv } from './ProgressBar.styled';
+
+//dummy progress bar
+const ProgressBar = () =>{
+
+    return(
+
+        <ProgressBarDiv />
+    );
+}
+
+export default ProgressBar;
