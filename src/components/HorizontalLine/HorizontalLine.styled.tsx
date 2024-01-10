32b0514612da11f4a40d8582e06ef2a8ee1627d4
Adam Avetisian <adamavetisian@Adams-iMac.local>
2024-01-10 08:04:27 -0800

    Implemment and horizontal line divider to separate contents

---
+++ src/components/HorizontalLine/HorizontalLine.styled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+export const HorizontalLineDiv = styled.div`
+
+    background-color:black;
+    width: 100%;
+    height:2px;
+    
+    
+`
---
+++ src/components/HorizontalLine/HorizontalLine.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HorizontalLineDiv } from "./HorizontalLine.styled";
+const HorizontalLine = () =>{
+
+    console.log("test")
+    return (
+
+        <HorizontalLineDiv />
+    );
+}
+
+export default HorizontalLine;
