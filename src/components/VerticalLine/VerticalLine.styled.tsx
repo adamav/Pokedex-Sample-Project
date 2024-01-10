6abfc5f8c44c04086795414dc3e99efdecb64caa
Adam Avetisian <adamavetisian@Adams-iMac.local>
2024-01-10 11:18:03 -0800

    Implememnt and vertical line component

---
+++ src/components/VerticalLine/VerticalLine.styled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+export const VerticalLineDiv = styled.div`
+
+    background-color:black;
+    height: 100%;
+    width:2px;
+    
+    
+`
---
+++ src/components/VerticalLine/VerticalLine.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {VerticalLineDiv} from './VerticalLine.styled';
+
+const VerticalLine = () =>{
+
+    return(
+
+        <VerticalLineDiv />
+    );
+}
+
+export default VerticalLine;
